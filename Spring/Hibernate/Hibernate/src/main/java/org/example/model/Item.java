78fa869c5d65756d0bbd524f91e566d0b6ceb276
RedPlanet3 <voshodsolnca@mail.ru>
2023-06-27 13:54:09 +0300

    hibernate

---
+++ Spring/Hibernate/Hibernate/src/main/java/org/example/model/Item.java
@@ -0,0 +1,60 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "item")
+public class Item {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    private Person owner;
+
+    @Column(name = "item_name")
+    private String item_name;
+
+    public Item() {
+    }
+
+    public Item(String item_name) {
+        this.item_name = item_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public String getItem_name() {
+        return item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", item_name='" + item_name + '\'' +
+                ", Person_name='" + owner.getName() + '\'' +
+                '}';
+    }
+}
---
+++ Spring/Hibernate/Hibernate/src/main/java/org/example/model/Movie.java
@@ -0,0 +1,92 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "movie")
+public class Movie {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "year_of_rpoduct")
+    private int year;
+
+    public List<Actor> getActorList() {
+        return actorList;
+    }
+
+    public void setActorList(List<Actor> actorList) {
+        this.actorList = actorList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Movie movie = (Movie) o;
+
+        if (id != movie.id) return false;
+        if (year != movie.year) return false;
+        return name != null ? name.equals(movie.name) : movie.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + year;
+        return result;
+    }
+
+    @ManyToMany(mappedBy = "movies")
+    private List<Actor> actorList;
+
+    public Movie() {
+    }
+
+    public Movie(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
